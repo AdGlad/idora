c12e799632c5d2d79f40cc7851f62a49b5d013ec
AdGlad <bagladstone@bigpond.com>
2018-08-09 01:19:38 +0000

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+  validates :username, presence: true , uniqueness: true, length: {minimum:3,maximum:30}
+  validates :email, presence: true , uniqueness: true, length: {minimum:3,maximum:130}
 end
