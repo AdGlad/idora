c97b9952877aa0518b1bc2b25cb996656c74adac
AdGlad <bagladstone@bigpond.com>
2018-08-09 07:22:08 +0000

    Added user edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def edit
+   @user = User.find(params[:id]) 
+  end
+  
+  def update
+   @user = User.find(params[:id]) 
+   if @user.update(user_params)
+    flash[:success]="The account #{@user.username},  have successfully updated your account"
+    redirect_to collections_path
+   else
+    render 'new'
+   end
+  
+  end
+  
   def create
    @user = User.new(user_params)
    if @user.save
